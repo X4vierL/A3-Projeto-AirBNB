2444f781a50efd9ef12022bd258e9507317749a3
Augusto D' Amices <damicesprogrammer@gmail.com>
2024-10-05 20:28:56 -0300

    Commit #2 VAI TOMA NO CU

--- cadastro-anuncios/cadastro-anuncios.php
+++ cadastro-anuncios/cadastro-anuncios.php
@@ -43,55 +43,100 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <title>A3 - TravelBnB</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
 <body>
 <div class="container">
-    <header>
-        <h1>Criação de Anúncios</h1>
+    <header class="header-main">
+         <div class="login-button">
+            <button><a href="http://localhost/A3---Projeto-AirBNB/logout.php"> <i class="fa-solid fa-right-from-bracket"></i>Logout </a></i></button>
+         </div>
     </header>
 
-    <nav class="sidebar">
-        <ul>
-            <li><a href="#">Item 1</a></li>
-            <li><a href="#">Item 2</a></li>
-            <li><a href="#">Item 3</a></li>
-        </ul>
-    </nav>
+    <div class="container">
+        <nav class="sidebar">
+            <div class="logo">
+                <i class="fa-regular fa-map"> Travel BnB</i>
+            </div>
+            <ul>
+                <li><a href="http://localhost/A3---Projeto-AirBNB/meus-anuncios/meus-anuncios.php"> <?php echo ($_SESSION["nivel_usuario"] == 'ADM') ? 'Anúncios pendentes.' : 'Meus anúncios'; ?> </a></li>
+            </ul>
+        </nav>
+    </div>
 
     <main class="main-container">
         <div class="form-container">
-            <h2>Formulário</h2>
-            <form action="cadastro-anuncios.php" method="POST" enctype="multipart/form-data">
-                <label for="hospedes">Quantidade de Hóspedes:</label>
-                <input type="number" id="hospedes" name="hospedes" required><br>
-
-                <label for="quarto">Número de Quartos:</label>
-                <input type="number" id="quarto" name="quarto" required><br>
-
-                <label for="camas">Número de Camas:</label>
-                <input type="number" id="camas" name="camas" required><br>
-
-                <label for="banheiros">Número de Banheiros:</label>
-                <input type="number" id="banheiros" name="banheiros" required><br>
-
-                <label for="contato">Contato (telefone):</label>
-                <input type="text" id="contato" name="contato" maxlength="20" required><br>
-
-                <label for="localidade">Localidade:</label>
-                <input type="text" id="localidade" name="localidade" maxlength="20" required><br>
-
-                <label for="valor">Valor (em reais):</label>
-                <input type="number" id="valor" name="valor" required><br>
-
-                <label for="imagem">Imagem do Imóvel:</label>
-                <input type="file" id="imagem" name="imagem" accept="image/*"><br>
-
-                <label for="descricao">Descrição:</label>
-                <textarea id="descricao" name="descricao" maxlength="255" required></textarea><br><br>
-
-                <input type="submit" value="Salvar">
+            <h2>Cadastre seu anúncio</h2>
+            <form action="cadastro-anuncios.php" method="POST" enctype="multipart/form-data" class="form-wrapper">
+                <div class="input-group-one-row">
+                    <label>
+                        <div class="input-style hover-effect">
+                            <input type="text" id="contato" name="contato" maxlength="20" placeholder="Contato (telefone)" required>
+                            <i class="fa-solid fa-phone"></i>
+                        </div>
+                    </label>
+                    <label>
+                        <div class="input-style hover-effect">
+                            <input type="text" id="localidade" name="localidade" maxlength="20" placeholder="Localidade" required>
+                            <i class="fa-solid fa-location-dot"></i>
+                        </div>
+                    </label>
+                    <label>
+                        <div class="input-style hover-effect">
+                            <input type="number" id="valor" name="valor" placeholder="Valor (em reais)" required>
+                            <i class="fa-solid fa-dollar-sign"></i>
+                        </div>
+                    </label>
+                </div>
+                <div class="input-group-two-row">
+                    <label>
+                        <div class="input-style hover-effect">
+                            <input type="number" id="hospedes" name="hospedes" placeholder="N° de Hóspedes" required>
+                            <i class="fa-solid fa-people-roof"></i>
+                        </div>
+                    </label>
+                    <label>
+                        <div class="input-style hover-effect">
+                            <input type="number" id="quarto" name="quarto" placeholder="N° de Quartos" required>
+                            <i class="fa-solid fa-restroom"></i>
+                        </div>
+                    </label>
+                </div>
+                <div class="input-group-two-row">
+                    <label>
+                        <div class="input-style hover-effect">
+                            <input type="number" id="camas" name="camas" placeholder="N° de Camas" required>
+                            <i class="fa-solid fa-bed"></i>
+                        </div>
+                    </label>
+                    <label>
+                        <div class="input-style hover-effect">
+                            <input type="number" id="banheiros" name="banheiros" placeholder="N° de Banheiros" required>
+                            <i class="fa-solid fa-toilet"></i>
+                        </div>
+                    </label>
+                </div>
+                <div class="input-group-one-row">
+                    <label>
+                        <div class="input-style text-area">
+                            <p>Descreva seu imóvel:</p>
+                            <textarea id="descricao" name="descricao" maxlength="255" required></textarea>
+                        </div>
+                    </label>
+                </div>
+                <div class="input-group-one-row">
+                    <label>
+                        <div class="input-style img">
+                            <p>Fotos do imóvel:</p>
+                            <input type="file" id="imagem" name="imagem" accept="image/*">
+                        </div>
+                    </label>
+                </div>
+                <div class="buttons">
+                    <a href="http://localhost/A3---Projeto-AirBNB/main-page/main-page.php">Voltar</a>
+                    <input type="submit" value="Criar Anúncio">
+                </div>
             </form>
-            <a href="http://localhost/A3---Projeto-AirBNB/main-page/main-page.php">Voltar</a>
         </div>
     </main>
 </div>
